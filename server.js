7da5f075a8bdb154f5efe69434a3d4a5a8efb412
raeland <rsvarady@gmail.com>
2024-03-01 22:14:23 -0500

    idk anymore who can say these days

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
  
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use("/", require("./routes"))
 
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.send("Hello World");
 });
  
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 8080));
 });
